43cb2123aa1e9f5c47468249586babcc33c63773
diego huamani chañi <dhuamanich@unsa.edu.pe>
2023-05-21 15:57:27 -0500

    cambiando  node.java

--- Node.java
+++ Node.java
@@ -1,49 +1,24 @@
-public class Node<T>{
-	private T data;
-	private Node<T> nextNode;
+public class Node<T> {
+    private T data;
+    private Node<T> nextNode;
 
-	public Node(T dataNodo, Node<T> next){
-		this.data = dataNodo;
-		this.nextNode = next;
-	}
+    public Node(T data) {
+        this.data = data;
+        this.nextNode = null;
+    }
 
-	public Node(T dataNodo){
-		this(dataNodo, null);
-	}
-	public Node(){
-		this(null, null);
-	}
-	public T getInfo(){
-		return this.data;
-	}
-	public Node<T> getNext(){
-		return this.nextNode;
-	}
-	public void setData(T dataNodo){
-		this.data = dataNodo;
-	}
-	public void setNext(Node<T> next){
-		this.nextNode = next;
-	}
+    public T getData() {
+        return data;
+    }
 
-	public void clear(){
-		this.data = null;
-		this.nextNode = null;
-	}
+    public void setData(T data) {
+        this.data = data;
+    }
+	public Node<T> getNextNode() {
+        return nextNode;
+    }
 
-	public boolean isEmpty(){
-		if(data == null){
-			return true;
-		}else{
-			return false;
-		}
-	}
-
-	public String toString(){
-		if(data != null){
-			return data.toString();
-		}else{
-			return null;
-		}
-	}
+    public void setNextNode(Node<T> nextNode) {
+        this.nextNode = nextNode;
+    }
 }
